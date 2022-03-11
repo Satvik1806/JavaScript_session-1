cfe39595a7070ff4faecc40d119e2b5cde7089dc
Satvik1806 <satvik0106@gmail.com>
2022-03-11 11:03:02 +0530

    code added

---
+++ script.js
@@ -0,0 +1,16 @@
+var len = 23
+console.log(len)
+var len = 23.7
+console.log(len)
+var len = "hehe"
+console.log(len)
+var len = false
+console.log("length is = 23 "+len)
+const pi = 3.14
+var dia = 4
+let rad = dia/2
+let AOC = pi*rad*rad
+let COC = 2*pi*rad
+console.log("Radius of circle = "+rad)
+console.log("Area of circle = "+AOC)
+console.log("Circumference of circle = "+COC)
